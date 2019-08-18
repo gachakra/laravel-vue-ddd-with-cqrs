e7c76b3f0e2c349f07673ef64c57c5d25d7121a0
Kangin Kim <gachakra-arounda.world@hotmail.com>
2019-08-18 21:16:36 +0900

    discard Docker npm container and make npm installed in php container so that able to call HMR as last command / change container name, php to app / register php-fpm to openrc (like service)

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+@php
+    /**
+     * Created by IntelliJ IDEA.
+     * User: gachakra
+     * Date: 2019-08-17
+     * Time: 19:57
+     */
+@endphp
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <title>laravel-vue-ddd-with-cqrs</title>
+</head>
+<body>
+<div id="app">
+    <example-component></example-component>
+</div>
+<script src=" {{ mix('/js/app.js') }} "></script>
+</body>
+</html>
