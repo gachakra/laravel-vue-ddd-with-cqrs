5f1b67a45111f0abdf2795fcfa8654864785e53d
Kangin Kim <gachakra.kangin@notonza.dev>
2020-01-04 16:01:51 +0900

    session driver file to redis / update init files including docker

--- laravel.generate-key.sh
+++ laravel.generate-key.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env ash
 
+if grep -qE "^APP_KEY=.+?$" ./.env; then
+    echo "Laravel app key already exists"
+fi
+
 # generate app key for laravel and set it root .env file
 if grep -qE "^APP_KEY=$" ./.env; then
     php artisan key:generate
