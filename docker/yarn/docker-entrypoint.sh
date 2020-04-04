0df249473d99aaaddb2cb4cf47e0fe3fc498fdbe
Kangin Kim <gachakra.kangin@notonza.dev>
2020-04-04 22:51:45 +0900

    Adopt docker-entrypoint.sh from composer image and fix Dockerfile in order to run `docker-compose run yarn {command}`

---
+++ docker/yarn/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env ash
+# Copied from https://github.com/composer/docker/blob/master/docker-entrypoint.sh
+
+isCommand() {
+  # Retain backwards compatibility with common CI providers,
+  if [ "$1" == "ash" ]; then
+    return 1
+  fi
+
+  yarn help $1 > /dev/null 2>&1
+}
+
+# check if the first argument passed in looks like a flag
+if [ "${1#-}" != "$1" ]; then
+  set -- /sbin/tini -- yarn "$@"
+# check if the first argument passed in is yarn
+elif [ "$1" = 'yarn' ]; then
+  set -- /sbin/tini -- "$@"
+# check if the first argument passed in matches a known command
+elif isCommand "$1"; then
+  set -- /sbin/tini -- yarn "$@"
+fi
+
+exec "$@"
